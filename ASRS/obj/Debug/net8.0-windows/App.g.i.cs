4ec5130e6fd3f232e974615ab0264744986f0a29
Denis Pyatirov <denis.pyatirov@gmail.com>
2025-04-24 14:32:38 +0300

    Common commit

--- ASRS/obj/Debug/net8.0-windows/App.g.i.cs
+++ ASRS/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ASRS {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ASRS {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             ASRS.App app = new ASRS.App();
             app.InitializeComponent();
